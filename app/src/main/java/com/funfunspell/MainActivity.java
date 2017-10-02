e2ed12df29ee5ef2e1a67f5fd7b5028bd4e60016
thcathy <thcathy@gmail.com>
2017-10-02 12:07:06 +0800

    fix login issue

--- app/src/main/java/com/funfunspell/MainActivity.java
+++ app/src/main/java/com/funfunspell/MainActivity.java
@@ -1,14 +1,11 @@
 package com.funfunspell;
 
 import android.app.ProgressDialog;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
-import android.view.Window;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -26,8 +23,11 @@ public class MainActivity extends AppCompatActivity {
         myWebView.setWebViewClient(new MyWebViewClient());
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        myWebView.getSettings().setUserAgentString("Mozilla/5.0 (Linux; Android 6.0.1; SM-G920V Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.36");
 
-        myWebView.loadUrl("http://www.funfunspell.com/practice/selfdictation/input.jsf");
+        myWebView.loadUrl("https://www.funfunspell.com/practice/selfdictation/input.jsf");
 
         progressDialog=new ProgressDialog(this);
         progressDialog.setMessage("Loading");
@@ -52,14 +52,7 @@ public class MainActivity extends AppCompatActivity {
         @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-            if (request.getUrl().getHost().equals("www.funfunspell.com")) {
-                // This is my web site, so do not override; let my WebView load the page
-                return false;
-            }
-            // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
-            Intent intent = new Intent(Intent.ACTION_VIEW, request.getUrl());
-            startActivity(intent);
-            return true;
+            return false;
         }
 
         @Override
